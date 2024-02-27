c602c75421bb388bd110bab7583871e18e5dc524
Sarmad <sarmadnaroo.bsiietkhpf20@iba-suk.edu.pk>
2024-02-27 22:07:24 +0500

    task 1

--- app/src/main/java/com/example/madlab/MainActivity.kt
+++ app/src/main/java/com/example/madlab/MainActivity.kt
@@ -1,92 +1,66 @@
-package com.example.madlab
+package com.example.myapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.madlab.ui.theme.MadLabTheme
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MadLabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-//                    Greeting("Android")
-                    CardRow()
-                }
+            MyApplicationTheme {
+                ButtonCent()
             }
         }
     }
 }
 
 @Composable
-fun CardRow() {
-    val padding = 16.dp
-    Row(verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .padding(padding)
-            .fillMaxWidth()
+fun ButtonCent(modifier: Modifier = Modifier){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val buttonColor = remember { mutableStateOf(Color.Red) }
+
+        Button(
+            onClick = {
+                buttonColor.value = Color.Green // Change color to green when clicked
+            },
+            modifier = Modifier.padding(16.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = buttonColor.value)
         ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "image",
-            modifier = Modifier
-                .clip(RoundedCornerShape(10.dp))
-                .requiredSize(150.dp)
-                .weight(2f)
-        )
-        Column (
-            modifier = Modifier.weight(1f)
-        ){
-            Text(text = "Iron Man",
-                modifier = Modifier
-                    .offset(x=4.dp)
-            )
-            Text(text = "Age: 43",
-                modifier = Modifier
-                    .paddingFromBaseline(top = 50.dp)
-                    .offset(x = 4.dp)
-            )
+            Text(text = "Add one glass")
         }
     }
 }
 
+
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
-    MadLabTheme {
-//        Greeting("Android")
-        CardRow()
+    MyApplicationTheme {
+        ButtonCent()
+
     }
 }
